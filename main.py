ae45ca17c943c527d155115fefc930cbce2957a4
Michael Brockus <55331536+michaelbrockus@users.noreply.github.com>
2021-01-30 00:20:28 -0800

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,14 @@ import sys
 
 
 def main():
-    print('Let’s get hacking!')
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+
+    answers = [[i, j, k] for i in range(x + 1) for j in range(y + 1) for k in range(z + 1) if i + j + k != n]
+    print(answers)
+
 
 if __name__ == '__main__':
     sys.exit(main())
